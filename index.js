dc09ca14819745ee2eb705f6021ec5aab21e339c
Kibur_Yoseph <kiburstgr@gmail.com>
2023-04-09 09:55:39 +0300

    Page Number Scroll Functional

---
+++ index.js
@@ -0,0 +1,41 @@
+const pageNumber = document.getElementById("pageNumber");
+let page = 1;
+const scrollUp = document.getElementById("scrollUp");
+const scrollDown = document.getElementById("scrollDown");
+const ACircle = Array.from(document.querySelectorAll('.ACircle'));
+const circleHeight = ACircle[1].style.height;
+
+function circleAnimation(element){
+    if(ACircle.indexOf(element) == (page - 1)){
+            element.style.height = `0.1em`;
+        }
+    if(ACircle.indexOf(element) != (page - 1)){
+        element.style.height = `${circleHeight}`
+    }
+}
+
+scrollDown.onclick = function () {
+    if(page < 6){
+        page++;
+        pageNumber.innerHTML = `0${page}`
+    }
+    if(page == 6){
+        page = 1;
+        pageNumber.innerHTML = `0${page}`
+    }
+    ACircle.forEach(circleAnimation);
+}
+
+ACircle.forEach(circleAnimation);
+
+scrollUp.onclick = function () {
+    if(page > 0){
+        page--;
+        pageNumber.innerHTML = `0${page}`
+    }
+    if(page == 0){
+        page = 5;
+        pageNumber.innerHTML = `0${page}`
+    }
+    ACircle.forEach(circleAnimation);
+}
